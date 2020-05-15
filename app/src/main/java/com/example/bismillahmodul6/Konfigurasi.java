bfcd5d9a172586edb7b7bb8c935bbbf3bafef4f1
muhammadfajar77 <64601760+muhammadfajar77@users.noreply.github.com>
2020-05-15 12:06:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bismillahmodul6/Konfigurasi.java
@@ -0,0 +1,23 @@
+package com.example.bismillahmodul6;
+
+public class Konfigurasi {
+    public static final String URL_ADD = "https://fajarruddin.000webhostapp.com/praktikum6/insert.php";
+    public static final String URL_GET_ALL = "https://fajarruddin.000webhostapp.com/praktikum6/read.php";
+    public static final String URL_GET_MHS = "https://fajarruddin.000webhostapp.com/praktikum6/select.php";
+    public static final String URL_UPDATE_MHS = "https://fajarruddin.000webhostapp.com/praktikum6/update.php";
+    public static final String URL_DELETE_MHS = "https://fajarruddin.000webhostapp.com/praktikum6/delete.php";
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Script PHP
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+    //ID Mahasiswa
+    //mhs itu singkatan dari Mahasiswa
+    public static final String MHS_ID = "mhs_id";
+}
